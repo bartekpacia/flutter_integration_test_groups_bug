f6eb9ddf3711748fc65c8bbc5e28eef64a94f9ed
Bartek Pacia <barpac02@gmail.com>
2023-01-19 00:15:28 +0100

    setup convenient_test (using its readme)

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,50 +1,66 @@
+import 'package:convenient_test_dev/convenient_test_dev.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_integration_test_groups_bug/main.dart';
+import 'package:flutter_integration_test_groups_bug/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  convenientTestMain(MyConvenientTestSlot(), () {
+    group('Counter', () {
+      testWidgets(
+        'is incremented when plus button is tapped',
+        (WidgetTester tester) async {
+          await tester.pumpWidget(const app.MyApp());
 
-  group('Counter', () {
-    testWidgets(
-      'is incremented when plus button is tapped',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(const MyApp());
+          expect(find.text('0'), findsOneWidget);
+          expect(find.text('1'), findsNothing);
 
-        expect(find.text('0'), findsOneWidget);
-        expect(find.text('1'), findsNothing);
+          await tester.pump(const Duration(seconds: 3));
 
-        await tester.pump(const Duration(seconds: 3));
+          await tester.tap(find.byIcon(Icons.add));
+          await tester.pump();
 
-        await tester.tap(find.byIcon(Icons.add));
-        await tester.pump();
+          await tester.pump(const Duration(seconds: 3));
 
-        await tester.pump(const Duration(seconds: 3));
+          expect(find.text('0'), findsNothing);
+          expect(find.text('1'), findsOneWidget);
+        },
+      );
 
-        expect(find.text('0'), findsNothing);
-        expect(find.text('1'), findsOneWidget);
-      },
-    );
+      testWidgets(
+        'is decremented when minus button is tapped',
+        (WidgetTester tester) async {
+          await tester.pumpWidget(const app.MyApp());
 
-    testWidgets(
-      'is decremented when minus button is tapped',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(const MyApp());
+          expect(find.text('0'), findsOneWidget);
+          expect(find.text('-1'), findsNothing);
 
-        expect(find.text('0'), findsOneWidget);
-        expect(find.text('-1'), findsNothing);
+          await tester.pump(const Duration(seconds: 3));
 
-        await tester.pump(const Duration(seconds: 3));
+          await tester.tap(find.byIcon(Icons.remove));
+          await tester.pump();
 
-        await tester.tap(find.byIcon(Icons.remove));
-        await tester.pump();
+          await tester.pump(const Duration(seconds: 3));
 
-        await tester.pump(const Duration(seconds: 3));
-
-        expect(find.text('0'), findsNothing);
-        expect(find.text('-1'), findsOneWidget);
-      },
-    );
+          expect(find.text('0'), findsNothing);
+          expect(find.text('-1'), findsOneWidget);
+        },
+      );
+    });
   });
+}
+
+class MyConvenientTestSlot extends ConvenientTestSlot {
+  @override
+  Future<void> appMain(AppMainExecuteMode mode) async => app.main();
+
+  @override
+  BuildContext? getNavContext(ConvenientTest t) =>
+      app.MyApp.navigatorKey.currentContext;
+}
+
+extension on ConvenientTest {
+  Future<void> myCustomCommand() async {
+    // Do anything you like... This is just a normal function
+    debugPrint('Hello, world!');
+  }
 }
